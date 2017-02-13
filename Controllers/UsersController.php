b43d4baf3097e732017cee8a1396efa8dd9be598
SleepWalker91 <hamed.sharifian@gmail.com>
2017-02-13 15:37:15 +0330

    user model added

--- Controllers/UsersController.php
+++ Controllers/UsersController.php
@@ -1,12 +1,24 @@
 <?php
 
 namespace app\controllers;
+use app\models\Users;
+use Yii;
 
 class UsersController extends \yii\web\Controller
 {
     public function actionIndex()
     {
         return $this->render('index');
+    }
+    
+    public function actionRegister(){
+    	$model=new Users();
+    	if($model->load(Yii::$app->request->post()) && $model->validate()) {
+            echo "validated";
+        } else {
+            echo "error";
+        }
+
     }
 
 }
