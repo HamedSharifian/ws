17bb3a514f5f57b88671c20ca37f8a18b1ec3368
SleepWalker91 <hamed.sharifian@gmail.com>
2017-04-22 16:03:35 +0430

    all deals

--- Controllers/StocksController.php
+++ Controllers/StocksController.php
@@ -12,12 +12,26 @@ class StocksController extends \yii\web\Controller
     }
     
     public function actionGetall(){
-    	$query = \app\models\Stocks::find()->limit(10);
-   		// get the total number of users
-  	 	//creating the pagination object
-   		$models = $query->all();
-   		var_dump($models);
-     	 echo json_encode($models);
+    	$model=new \app\models\Stocks();
+	 $model->scenario= \app\models\Stocks::SCENARIO_GET_BEAST_DEALS;
+    	 if($model->load(Yii::$app->request->get())) {
+           if($model->validate()){
+              
+    	       $models= \app\models\Stocks::find()
+                       ->where(["storeTo"=>$model->storeTo])
+                       ->andWhere(["is not","discount",null])
+                       ->orderBy(['discount' => SORT_DESC])->all();
+               
+               ErrorManager::encodeAndReturn(200, null, $models);
+
+               return;
+           }// validation error
+           $errorInfos=ErrorManager::getErrorObjects($model->getErrors());
+           ErrorManager::encodeAndReturn(-1,$errorInfos,null);
+           return;
+        } 
+        ErrorManager::encodeHttpError(400);
+        return;
     } 
     
     public function actionGetbest(){
